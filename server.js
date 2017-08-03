00a33fcaa8a0b153e17d25acd2d33d77421972c0
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-03 23:42:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:'IMAD | A1',
+    heading:'article one',
+    date:'3 aug 17',
+    content:`
+            <p>
+                Globalization (or globalisation; see spelling differences) refers to the free movement of goods, capital, services, people, technology and information. It is the action or procedure of international integration of countries arising from the convergence of world views
+            </p>
+            <p>
+                A business (also known as an enterprise, a company or a firm) is an organizational entity involved in the provision of goods and services to consumers.
+            </p>`
+};    
+
+function createtemplate(data){
+    date=data.date;
+    title=data.title;
+    heading=data.heading;
+    content=data.content;
+    var htmltemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+      <div class="cont">
+        <div>
+            ${date}
+        </div>
+        <div>
+            <a href="http://www.outlooker.in/">click</a>
+        </div>
+        <hr/>
+        <div>
+            <h2>
+                ${heading}
+            </h2>
+        </div>
+        <div> 
+           ${content}
+        </div>
+      </div>
+    </body>
+</html>`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
