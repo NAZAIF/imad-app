4b79f9cb737aad61a44dc8d6b1ed331accae2a64
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-05 21:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('main', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:artname', function (req,res) {
     var artname=req.params.artname;
     res.send(createtemplate(articles[artname]));  
