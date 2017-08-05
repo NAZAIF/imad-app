20b1adbd6dc3965249fc5fa7a7dc3e7d70507348
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-05 23:16:20 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('ITs me nazaif');
 var elmt = document.getElementById('maintext');
 elmt.innerHTML = 'new mode';
+var marginl = 0;
+function goright(){
+    marginl = marginl+1;
+    img.style.marginl = marginl + "px";
+}
+var img = document.getElemetById('madi');
+img.onclick  = function() {
+    var interval = setInterval(goright,50);
+}
