e1753c669063dd476a736631cea884ad828d3185
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-07 22:53:29 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
-var button = document.getElementById(counter);
+var button = document.getElementById('counter');
 button.onclick = function (){
     counter = counter + 1;
-    var span = document.getElementById(count);
+    var span = document.getElementById('count');
     span.innerHTML = counter.toSting();
 };
