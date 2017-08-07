cc01256f07abd3c4e509e7b3f108cff8c6302d16
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-07 22:16:31 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,6 @@
-console.log('ITs me nazaif');
-var elmt = document.getElementById('maintext');
-elmt.innerHTML = 'new mode';
-var marginLeft = 0;
-var img = document.getElementById('madi');
-function goright(){
-    marginLeft = marginLeft+2;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick  = function() {
-    var interval = setInterval(goright,50);
+var button = document.getElementById(counter);
+button.onclick = function (req, res){
+    counter = counter + 1;
+    var span = document.getElementById(count);
+    span.innerHTML = counter.toSting();
 };
