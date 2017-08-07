80f4fe9006b457cb3e7fde94a772c967f02a153d
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-07 21:47:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -80,6 +80,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -95,12 +101,6 @@ app.get('/:artname', function (req,res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var counter =0;
-app.get('/counter', function (req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
 });
 
 
