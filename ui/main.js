b39748ce94409c7116add6b1e89781747a6101af
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-07 22:52:47 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button = document.getElementById(counter);
-button.onclick = function (req, res){
+button.onclick = function (){
     counter = counter + 1;
     var span = document.getElementById(count);
     span.innerHTML = counter.toSting();
