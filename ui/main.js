3a7157a26930a8f54605668e3f4300b30b0e0bef
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-10 16:33:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,27 @@ var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit = document.getElementById('subbtn');
 submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++){
+                list+='<li>'+names[i]+'</li>';
+                }
+                var ui = document.getElementById('namelist');
+                ui.innerHTML = list;
+            
+            }
+        }
+    };
+    
+    request.open('GET','http://nazaifmoid.imad.hasura-app.io/submit?name=' + name, true);
+    request.send(null);
+    
     var names = ['johny' , 'baby' , 'joy'];
     var list = '';
     for (var i=0; i<names.length; i++){
