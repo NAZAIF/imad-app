48d8aadb3b2d6271e06e834b1cf55f4d9357b81f
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-23 10:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'nazaifmoid',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB-PASSWORD
+    password: 'db-nazaifmoid-6415'
 };
 var app = express();
 app.use(morgan('combined'));
