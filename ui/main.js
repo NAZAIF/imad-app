42658380cc0eee0fea61c17a225080b25006d27f
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-26 20:42:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,46 +1,27 @@
-var button = document.getElementById('counter');
-button.onclick = function (){
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE){
-            if (request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-
-            }
-        }
-    };
-    
-    request.open('GET','http://nazaifmoid.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
-};
 
 
-
-var submit = document.getElementById('subbtn');
+var submit = document.getElementById('sbmtbtn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
         request.onreadystatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i<names.length; i++){
-                list+='<li>'+names[i]+'</li>';
+                alert('logged in successfully');
+                }else if(request.status === 403){
+                    alert('password/username is incorrect');
+                }else if(request.status === 500){
+                    alert('something wrong');
                 }
-                var ui = document.getElementById('namelist');
-                ui.innerHTML = list;
-            
-            }
+                
         }
+        
     };
-    var nameinput = document.getElementById('name');
-    var name = nameinput.value;
-    request.open('GET','http://nazaifmoid.imad.hasura-app.io/submit?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://nazaifmoid.imad.hasura-app.io/login' + name, true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
     
  };
