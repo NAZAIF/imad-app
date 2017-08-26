53bc2eb853f821324c52d4728da1a5c5a8a18731
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-08-26 19:29:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/testdb', function (req,res){
 
 function hash(input,salt){
     var hashd = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return [pbkdgf2, "10000", salt, hashd.toString('hex')].join('$');
+    return ["pbkdgf2", "10000", salt, hashd.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req,res){
