2a0484b11e05262fa5eeaeb8bfdab05955656b2f
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-09-02 08:01:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post('/create-user', function(req,res){
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbstring], function(err,result){
         if(err){
             res.setHeader('Content-Type','application/json');
-            res.status(500).send(JSON.parse('{"error":+ err.toString()}'));
+            res.status(500).send(JSON.parse('{"error": 'err.toString()'}'));
             //res.status(500).send(err.toString());
         }else{
             //res.send('user successfully created with ' + username);
