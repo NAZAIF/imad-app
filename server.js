45867dfc3eb5d5dd694b9679156476074c24f13e
Moideen Nazaif V M <nazaifmoid@gmail.com>
2017-09-06 13:58:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/getarticles', function (req,res) {
                 var artData = result.rows[0];
                 //var artobj = createtemplate(artData);
                 res.setHeader('Content-Type','application/json');
-                var json2 = JSON.strigify({id:artData.id});
+                var json2 = JSON.strigify({id:result.rows[1].id});
                 res.send(JSON.parse(json2));
                 
             }
